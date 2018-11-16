c05605472bfcf4c8748061aec7a6e25e34d2fa4a
KYJ <yzoo1008@snu.ac.kr>
2018-11-16 14:35:32 +0900

    update test.py

--- test.py
+++ test.py
@@ -1,20 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+#
+# data_dir = './data/'
+# P9_WOPR = 'P9_WOPR.txt'
+# DAY = 'days.dat'
+#
+# production_path = data_dir + P9_WOPR
+# day_path = data_dir + DAY
+#
+# production = np.loadtxt(production_path)
+# day = np.loadtxt(day_path)
+#
+# print(np.shape(production[:, 0]))
+# print(np.shape(day))
+#
+# for idx in range(0, 103):
+#     plt.figure(1)
+#     plt.plot(day, production[:, idx])
+# plt.show()
 
-data_dir = './data/'
-P9_WOPR = 'P9_WOPR.txt'
-DAY = 'days.dat'
+a = [[1, 2, 3],
+     [4, 6, 8],
+     [7, 10, 13]]
 
-production_path = data_dir + P9_WOPR
-day_path = data_dir + DAY
+np_a = np.array(a)
 
-production = np.loadtxt(production_path)
-day = np.loadtxt(day_path)
+np_a_min = np.reshape(np_a.min(axis=1), (np.shape(np_a.min(axis=1))[0], 1))
+np_a_max = np.reshape(np_a.max(axis=1), (np.shape(np_a.max(axis=1))[0], 1))
 
-print(np.shape(production[:, 0]))
-print(np.shape(day))
+print(np_a_min)
+print(np_a_max)
+print(np_a - np_a_min)
 
-for idx in range(0, 103):
-    plt.figure(1)
-    plt.plot(day, production[:, idx])
-plt.show()
+print((np_a - np_a_min)/(np_a_max-np_a_min + 1e-7))
